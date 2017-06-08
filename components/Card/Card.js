aaeadc88ebb52c807d2557964606462a30856f6b
Augusto Santos <augusto_santos55@hotmail.com>
2017-06-08 09:14:47 -0300

    update conteudo style

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -4,15 +4,15 @@ import s from './Card.css'
 class Card extends Component{
     render(){
         return(
-            <div className="wrapper-Card">
-                <div className="card_img" style={{ backgroundImage: `url(${this.props.img})`}} ></div>
-                <div className="card_info">
-                    <div className="card_info_title">Teste</div>
-                    <div className="card_info_data">07/06/2017</div>
-                    <div className="card_info_description"></div>
+            <div className={`${s.wrapper}`}>
+                <div className={`${s.bg}`} style={{ backgroundImage: `url(${this.props.img})`}} ></div>
+                <div className={`${s.info}`}>
+                    <div className={`${s.data}`} >07/06/2017</div>
+                    <div className={`${s.title}`}>Teste</div>
+                    <div className={`${s.description}`} ></div>
                 </div>
-                <div className="card_action">
-                    <a href="#" className="card_action_more">Var Mais</a>
+                <div className={`${s.action}`}>
+                    <a href="#" className={`${s.more}`}>Var Mais</a>
                 </div>
             </div>
         )
